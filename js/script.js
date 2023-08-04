4053c78e48362a9b82a91665ad354bcad5137642
JoeyBernardino <johoneybernardino@gmail.com>
2023-08-04 21:17:07 +0800

    Add all files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//   carousel //
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  const slides = document.querySelectorAll('.slides img');
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex].style.display = 'block';
+}
